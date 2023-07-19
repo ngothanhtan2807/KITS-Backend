a75d590384c324f21e069243f100d8d8090ae38d
Ngo Thanh Tan <ngothanhtan2608@gmail.com>
2023-07-19 14:40:33 +0700

    feature/product

--- src/main/java/com/kits/ecommerce/services/impl/ProductServiceImpl.java
+++ src/main/java/com/kits/ecommerce/services/impl/ProductServiceImpl.java
@@ -122,15 +122,17 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public ProductDto updateProduct(ProductDto productDto, Integer generalId) {
+    public ProductDto updateProduct(ProductDto productDto, Integer productId) {
         try {
             Catalog catalog = catalogRepo.findById(productDto.getCatalogID()).orElseThrow(() -> new ResoureNotFoundException("Catalog", "ID", productDto.getCatalogID()));
+
+           //lấy ảnh của productDto
             Set<MultipartFile> files = productDto.getFiles();
-
-            Product product0 = productRepo.findById(generalId).orElseThrow(() -> new ResoureNotFoundException("General", "ID", generalId));
-
+//product gốc
+            Product product0 = productRepo.findById(productId).orElseThrow(() -> new ResoureNotFoundException("Product", "ID", productId));
+//ds ảnh của product gốc
             List<ImageProduct> imageProductList = product0.getListImage();
-
+//xóa ảnh trong db
             for (ImageProduct image : imageProductList) {
                 image.setProduct(null);
                 imageProductRepo.save(image);
@@ -161,7 +163,21 @@ public class ProductServiceImpl implements ProductService {
 
                 product.addProductImages(imageProduct);
             }
+            Set<Color> colors = new HashSet<>();
+            List<Integer>colorID = productDto.getColorsID();
+            for (Integer i: colorID) {
+                colors.add(colorRepo.findById(i).orElseThrow(()->new ResoureNotFoundException("Color", "ID", i)));
+            }
+            product.setColors(colors);
+
+            List<Size>sizes = new ArrayList<>();
+            List<Integer>sizesID = productDto.getSizesID();
+            for(Integer i : sizesID){
+                sizes.add(sizeRepo.findById(i).orElseThrow(()-> new ResoureNotFoundException("Size", "ID", i)));
+            }
+            product.setSizes(sizes);
             product.setCatalog(catalog);
+
             productRepo.save(product);
 
             return this.convertToProductDto(product);
