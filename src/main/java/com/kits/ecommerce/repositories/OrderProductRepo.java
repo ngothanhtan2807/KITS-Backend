5e53a15dd8933f15e7691bc6c17d5a0901c55c28
ngothanhtan2807 <72880230+ngothanhtan2807@users.noreply.github.com>
2023-07-27 22:18:22 +0700

    update hot saler

--- src/main/java/com/kits/ecommerce/repositories/OrderProductRepo.java
+++ src/main/java/com/kits/ecommerce/repositories/OrderProductRepo.java
@@ -1,9 +1,17 @@
 package com.kits.ecommerce.repositories;
 
 import com.kits.ecommerce.entities.OrderProduct;
+import com.kits.ecommerce.entities.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface OrderProductRepo extends JpaRepository<OrderProduct, Integer> {
+
+//    @Query("select o.product from OrderProduct o group by o.product.id order by sum (o.quantity) DESC limit 5")
+//   @Query(value = "", nativeQuery = true)
+//    List<Product> hotSaler();
 }
